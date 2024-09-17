f9985d40305b418fe20dd8d77f3643bef7e0fdc0
Jared García <ljared.31g@gmail.com>
2024-09-17 10:24:12 -0600

    feat: added navigation links with gates pero user role

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -15,6 +15,14 @@
                     <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                         {{ __('Dashboard') }}
                     </x-nav-link>
+                    @can('schedule-class')
+                        <x-nav-link :href="route('schedule.create')" :active="request()->routeIs('schedule.create')">
+                            {{ __('Schedule a Class') }}
+                        </x-nav-link>
+                        <x-nav-link :href="route('schedule.index')" :active="request()->routeIs('schedule.index')">
+                            {{ __('Upcoming Classes') }}
+                        </x-nav-link>
+                    @endcan
                 </div>
             </div>
 
@@ -69,6 +77,12 @@
         <div class="pt-2 pb-3 space-y-1">
             <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
                 {{ __('Dashboard') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('schedule.create')">
+                {{ __('Schedule a Class') }}
+            </x-responsive-nav-link>
+            <x-responsive-nav-link :href="route('dashboard')" :active="request()->routeIs('schedule.index')">
+                {{ __('Upcoming Classes') }}
             </x-responsive-nav-link>
         </div>
 
