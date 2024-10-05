a699c31bbe1bdad234e62cf27b1c18bcb49a0ba5
Jared García <ljared.31g@gmail.com>
2024-10-05 12:12:13 -0600

    feat: created email and sending it for every canceled class

--- app/Listeners/NotifyClassCanceled.php
+++ app/Listeners/NotifyClassCanceled.php
@@ -3,9 +3,11 @@
 namespace App\Listeners;
 
 use App\Events\ClassCanceled;
+use App\Mail\ClassCanceledMail;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Facades\Mail;
 
 class NotifyClassCanceled
 {
@@ -22,7 +24,15 @@ class NotifyClassCanceled
      */
     public function handle(ClassCanceled $event): void
     {
-        $scheduledClass = $event->scheduledClass;
-        Log::info('Class canceled: ' . $scheduledClass);
+        $members = $event->scheduledClass->members;
+        $className = $event->scheduledClass->classType->name;
+        $classDateTime = $event->scheduledClass->date_time;
+        $details = compact('className', 'classDateTime');
+        $members->each(function ($member) use($details) {
+            // Send an email to the member
+            Mail::to($member)->send(new ClassCanceledMail(
+                $details
+            ));
+        });
     }
 }
---
+++ resources/views/emails/class-canceled.blade.php
@@ -0,0 +1,5 @@
+<p>Hey there,</p>
+<p>
+    Your {{ $details['className'] }} class on {{ $details['classDateTime']->format('jS M Y') }} at {{ $details['classDateTime']->format('g:i a') }} has been canceled by the instructor.
+    If you have any questions, please contact the instructor.
+</p>
